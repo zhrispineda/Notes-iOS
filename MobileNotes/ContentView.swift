74a0ce97b47856b35d4ad5e4a21585a17cec8de3
Chris <zhris@zhr.is>
2024-11-15 13:47:03 -0600

    Initial Commit

---
+++ MobileNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileNotes
+//
+//  Created by Chris on 11/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileNotes/MobileNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileNotesApp.swift
+//  MobileNotes
+//
+//  Created by Chris on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
