2a35711ab4d4693460c0dc555e4825f1c93b007c
Chris <zhris@zhr.is>
2024-11-17 11:41:21 -0600

    Added ToolbarItems.

--- MobileNotes/ContentView.swift
+++ MobileNotes/ContentView.swift
@@ -8,12 +8,37 @@ import SwiftUI
 struct ContentView: View {
     // Variables
     @AppStorage("ShouldShowWelcomeScreen") private var shouldShowWelcomeScreen = true
+    @State private var internalDebugging = false
+    @State private var searchText = String()
     
     var body: some View {
         NavigationStack {
             if !shouldShowWelcomeScreen {
                 List {
-                    EmptyView()
+                }
+                .navigationTitle("Folders")
+                .navigationBarTitleDisplayMode(.large)
+                .searchable(text: $searchText, placement: .toolbar)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        EditButton()
+                    }
+                    ToolbarItem(placement: .bottomBar) {
+                        Button {} label: {
+                            Image(systemName: "folder.badge.plus")
+                        }
+                    }
+                    ToolbarItem(placement: .bottomBar) {
+                        if internalDebugging {
+                            Button {} label: {
+                                Image(systemName: "gear")
+                            }
+                        } else {
+                            Button {} label: {
+                                Image(systemName: "square.and.pencil")
+                            }
+                        }
+                    }
                 }
             }
         }
